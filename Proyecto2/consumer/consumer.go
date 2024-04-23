85fc472b0e6b25d02fdc9ab76cb31a9bea1fbf7f
rubenralda <rubenralda@gmail.com>
2024-04-23 15:03:41 -0600

    add: conexion con mongo y registro de datos de prueba

--- Proyecto2/consumer/consumer.go
+++ Proyecto2/consumer/consumer.go
@@ -1,22 +1,49 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"time"
 
 	"github.com/confluentinc/confluent-kafka-go/v2/kafka"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 type Data struct {
+	Name  string `bson:"name,omitempty"`
+	Album string `bson:"album,omitempty"`
+	Year  string `bson:"year,omitempty"`
+	Rank  string `bson:"rank,omitempty"`
+}
+type Logs struct {
 	Name  string
-	Album string
-	Year  string
-	Rank  string
+	Fecha time.Time
 }
 
 func main() {
-	fmt.Println("Version 4.1")
+	fmt.Println("Version 5.1")
+	// Conexion mongo
+	uri := "mongodb://admin:1234@34.66.138.214:27017/?authSource=admin"
+
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(uri))
+	if err != nil {
+		panic(err)
+	}
+	defer func() {
+		if err = client.Disconnect(context.TODO()); err != nil {
+			panic(err)
+		}
+	}()
+	fmt.Println("Conexion MONGO realizada")
+	coll := client.Database("proyecto2").Collection("logs")
+	newLogs := Logs{Name: "Prueba1", Fecha: time.Now().Local()}
+	result, err := coll.InsertOne(context.TODO(), newLogs)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Ingresado", result)
 	consumer, err := kafka.NewConsumer(&kafka.ConfigMap{
 		"bootstrap.servers": "my-cluster-kafka-bootstrap:9092",
 		"group.id":          "grcp_producer",
